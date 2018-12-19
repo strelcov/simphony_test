be45f688736100a034b86767391dee4ef8e9e2aa
strelcov <sanyka-91@mail.ru>
2018-12-19 18:17:39 +0300

    start making api test

--- tests/AppBundle/Controller/ApiControllerTest.php
+++ tests/AppBundle/Controller/ApiControllerTest.php
@@ -7,83 +7,138 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 class ApiControllerTest extends WebTestCase
 {
 
-    /**
-     * TODO: мб можно сделать через dataProvider
-     */
     public function testAuthError()
     {
         $client = static::createClient();
-        $urls = [
-            '/api/v1/books/add',
-            '/api/v1/books/add?apikey=1112121121',
+        $variants = [
+            ['url' => '/api/v1/books/add', 'params' => []],
+            ['url' => '/api/v1/books/add', 'params' => ['apikey' => '1112121121']],
         ];
-        foreach ($urls as $url) {
-            $client->request('GET', $url);
+        foreach ($variants as $variant) {
+            $client->request('POST', $variant['url'], $variant['params']);
             $response = $client->getResponse();
-            $responseContent = $response->getContent();
-            $this->assertContains('error', $responseContent);
-            $this->assertContains('api key', $responseContent);
             $responseCode = $response->getStatusCode();
             $this->assertEquals(401, $responseCode);
+            $responseContent = $response->getContent();
+            $this->assertContains('error', $responseContent, 'Не найдено упоминание об ошибке в теле ответа');
+            $this->assertContains('api key', $responseContent, 'Не найдено упоминание об api key в теле ответа');
         }
     }
 
     public function testAddBookByApi()
     {
         $client = static::createClient();
-        $client->request('GET', '/api/v1/books/add');
-        $response = $client->getResponse();
-        $responseContent = $response->getContent();
-        $this->assertContains('error', $responseContent);
-        $this->assertContains('api key', $responseContent);
-        $responseCode = $response->getStatusCode();
-        $this->assertEquals(401, $responseCode);
+        $apikey = $client->getKernel()->getContainer()->getParameter('apikey');
+        //TODO: реструктурировать массив, чтобы можно было пройти по всем успешным и не успешным
+        $paramsVariants = [
+            [
+                'Не получилось вставить запись со всеми параметрами' => [
+                    'success' => [
+                        'apikey' => $apikey,
+                        'author' => 1,
+                        'title' => 'test book scs',
+                        'allowDownload' => 1,
+                        'readDate' => 2018-10-10,
+                    ]
+                ]
+            ],
+            [
+                'Не получилось вставить запись без allowDownload' => [
+                    'error' => [
+                        'apikey' => $apikey,
+                        'author' => 1,
+                        'title' => 'test book scs',
+                        'readDate' => 2018-10-10,
+                    ]
+                ]
+            ],
+            [
+                'Получилось вставить запись с несуществующим автором' => [
+                    'error' => [
+                        'apikey' => $apikey,
+                        'author' => -1,
+                        'title' => 'test book',
+                        'allowDownload' => 1,
+                        'readDate' => 2018-10-10,
+                    ]
+                ]
+            ],
+            [
+                'Получилось вставить запись без названия книги' => [
+                    'error' => [
+                        'apikey' => $apikey,
+                        'author' => 1,
+                        'allowDownload' => 1,
+                        'readDate' => 2018-10-10,
+                    ]
+                ]
+            ],
+            [
+                'Получилось вставить запись без автора' => [
+                    'error' => [
+                        'apikey' => $apikey,
+                        'title' => 'test book',
+                        'allowDownload' => 1,
+                        'readDate' => 2018-10-10,
+                    ]
+                ]
+            ],
+            [
+                'Получилось вставить запись без даты прочтения' => [
+                    'error' => [
+                        'apikey' => $apikey,
+                        'author' => 1,
+                        'title' => 'test book',
+                        'allowDownload' => 1,
+                    ]
+                ]
+            ],
+            [
+                'Получилось вставить запись с неправильным форматом даты прочтения' => [
+                    'error' => [
+                        'apikey' => $apikey,
+                        'author' => 1,
+                        'title' => 'test book',
+                        'allowDownload' => 1,
+                        'readDate' => 10-10-2018,
+                    ]
+                ]
+            ],
+            [
+                'Получилось вставить запись с пустым заголовком' => [
+                    'error' => [
+                        'apikey' => $apikey,
+                        'author' => 1,
+                        'title' => '',
+                        'allowDownload' => 1,
+                        'readDate' => 2018-10-10,
+                    ]
+                ]
+            ],
+
+        ];
+        foreach ($paramsVariants as $paramsVariant) {
+            //TODO: разделение на правильные и неправильные кейсы
+            $client->request('GET', '/api/v1/books/add', $params);
+            $response = $client->getResponse();
+            $this->assertEquals(400, $response->getStatusCode());
+            $responseContent = $response->getContent();
+            $this->assertContains('error', $responseContent);
+            $this->assertContains('must use method post', $responseContent);
+
+            $client->request('POST', '/api/v1/books/add', $params);
+
+            $response = $client->getResponse();
+            $responseCode = $response->getStatusCode();
+            $this->assertEquals(200, $responseCode);
+
+            $responseContent = $response->getContent();
+            //десериализуем возвращаемый объект и сравниваем его поля с параметрами, кототрые передали в него
+
+            //$this->assertContains('error', $responseContent);
+            //$this->assertContains('api key', $responseContent);
+        }
+
     }
 
-    /*
-    public function testCompleteScenario()
-    {
-        // Create a new client to browse the application
-        $client = static::createClient();
-
-        // Create a new entry in the database
-        $crawler = $client->request('GET', '/book/');
-        $this->assertEquals(200, $client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET /book/");
-        $crawler = $client->click($crawler->selectLink('Create a new entry')->link());
-
-        // Fill in the form and submit it
-        $form = $crawler->selectButton('Create')->form(array(
-            'appbundle_book[field_name]'  => 'Test',
-            // ... other fields to fill
-        ));
-
-        $client->submit($form);
-        $crawler = $client->followRedirect();
-
-        // Check data in the show view
-        $this->assertGreaterThan(0, $crawler->filter('td:contains("Test")')->count(), 'Missing element td:contains("Test")');
-
-        // Edit the entity
-        $crawler = $client->click($crawler->selectLink('Edit')->link());
-
-        $form = $crawler->selectButton('Update')->form(array(
-            'appbundle_book[field_name]'  => 'Foo',
-            // ... other fields to fill
-        ));
-
-        $client->submit($form);
-        $crawler = $client->followRedirect();
-
-        // Check the element contains an attribute with value equals "Foo"
-        $this->assertGreaterThan(0, $crawler->filter('[value="Foo"]')->count(), 'Missing element [value="Foo"]');
-
-        // Delete the entity
-        $client->submit($crawler->selectButton('Delete')->form());
-        $crawler = $client->followRedirect();
-
-        // Check the entity has been delete on the list
-        $this->assertNotRegExp('/Foo/', $client->getResponse()->getContent());
-    }
-
-    */
 }
