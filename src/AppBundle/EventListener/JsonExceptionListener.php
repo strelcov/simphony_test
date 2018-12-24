04f72d69d3da77e18c02b3e6b6c466436a3b2e5a
strelcov <sanyka-91@mail.ru>
2018-12-24 11:25:47 +0300

    add condition in listener for json exceptions for api

--- src/AppBundle/EventListener/JsonExceptionListener.php
+++ src/AppBundle/EventListener/JsonExceptionListener.php
@@ -14,7 +14,9 @@ class JsonExceptionListener
     {
         $request = $event->getRequest();
         $routeName = $request->get('_route');
-        if (stripos($routeName, 'api_book_') !== false) {
+        if (stripos($routeName, 'api_book_') !== false
+                || (!empty($request->server->all()['DOCUMENT_URI'])
+                    && stripos($request->server->all()['DOCUMENT_URI'], '/api/v1/') !== false)) {
             $exception = $event->getException();
             $customResponse = new JsonResponse(['error' => ['message' => $exception->getMessage()]], 500);
             $event->setResponse($customResponse);
